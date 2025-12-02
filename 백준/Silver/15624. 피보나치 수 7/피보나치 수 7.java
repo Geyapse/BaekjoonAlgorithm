1dcf24eb5ed2cb4816dfae2cd2383f5a3c842993
Geyapse <113494802+Geyapse@users.noreply.github.com>
2025-12-03 01:40:29 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 192 ms, Memory: 25320 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.java
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.java
@@ -5,13 +5,13 @@ public class Main {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		long[] dp = new long[n+1];
-		if(n<2){
+		if(n==0){
 			System.out.println(0);
 			return;
 		}
+		dp[0] = 0;
 		dp[1] = 1;
-		dp[2] = 1;
-		for(int i = 3; i <= n; i++){
+		for(int i = 2; i <= n; i++){
 			dp[i] = dp[i-1] + dp[i-2];
 			if(dp[i]>=1000000007){
 				dp[i]-=1000000007;
