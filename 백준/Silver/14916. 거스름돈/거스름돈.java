9a1576d03f1f430e08e63b20cc996041eb06e8e8
Geyapse <113494802+Geyapse@users.noreply.github.com>
2025-12-02 18:28:22 +0900

    [Silver V] Title: 거스름돈, Time: 176 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int n = in.nextInt();
+
+		int count = 0;
+
+		while (n >= 0) {
+			if (n % 5 == 0) {
+				count += n / 5;
+				System.out.println(count);
+				return;
+			}
+
+			n -= 2;
+			count++;
+		}
+
+		System.out.println(-1);
+	}
+}
