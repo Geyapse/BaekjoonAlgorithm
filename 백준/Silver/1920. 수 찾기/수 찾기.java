0824f30083f9f2b7a0779c02e6fc62052a4317d2
Geyapse <113494802+Geyapse@users.noreply.github.com>
2025-12-03 20:47:01 +0900

    [Silver IV] Title: 수 찾기, Time: 1712 ms, Memory: 185212 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] arr = new int[n];
+
+		for(int i = 0; i < n; i++){
+			arr[i] = in.nextInt();
+		}
+		Arrays.sort(arr);
+		int m = in.nextInt();
+
+		for(int i = 0; i < m; i++){
+			int target = in.nextInt();
+			if(binarySearch(arr, target)){
+				System.out.println(1);
+			}else{
+				System.out.println(0);
+			}
+		}
+
+		}
+
+	public static boolean binarySearch(int[] arr, int target) {
+		int left = 0;
+		int right = arr.length - 1;
+
+		while (left <= right) {
+			int mid = (left + right) / 2;
+
+			if (arr[mid] == target) {
+				return true;
+			} else if (arr[mid] < target) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		return false;
+	}
+
+}
