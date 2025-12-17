7dc8c1bc95cacf021c41fb568c49bc3fc99a15f0
Geyapse <113494802+Geyapse@users.noreply.github.com>
2025-12-17 14:11:58 +0900

    [Silver III] Title: 계단 오르기, Time: 204 ms, Memory: 18036 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] dp = new int[n + 1];
+		int[] score = new int[n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			score[i] = in.nextInt();
+		}
+
+		if(n==1){
+			System.out.println(score[1]);
+			return;
+		}
+
+		dp[0] = 0;
+		dp[1] = score[1];
+		dp[2] = score[1] + score[2];
+
+		for (int i = 3; i <= n; i++) {
+			dp[i] = Math.max(dp[i-2] + score[i], dp[i-3] + score[i-1] + score[i]);
+		}
+
+		System.out.println(dp[n]);
+	}
+}
