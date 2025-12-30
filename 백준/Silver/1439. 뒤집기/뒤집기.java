312d039314eaa38f053c292321f04e4a198d7649
Geyapse <113494802+Geyapse@users.noreply.github.com>
2025-12-30 19:29:53 +0900

    [Silver V] Title: 뒤집기, Time: 200 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String s = in.nextLine();
+		int count = 0;
+		for (int i = 0; i < s.length()-1; i++) {
+			if(s.charAt(i) != s.charAt(i+1)) {
+				count++;
+			}
+		}
+		System.out.println((count+1)/2);
+	}
+}
