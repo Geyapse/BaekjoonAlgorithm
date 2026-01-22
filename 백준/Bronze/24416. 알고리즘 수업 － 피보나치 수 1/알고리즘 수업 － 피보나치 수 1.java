d7e861d092dc16ba90fedf8ce6127e61339b9c10
Geyapse <113494802+Geyapse@users.noreply.github.com>
2026-01-22 19:27:17 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 420 ms, Memory: 18392 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -6,14 +6,14 @@ public class Main {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		fib(n);
-		System.out.println((count+1) + " " + (n - 2));
+		System.out.println(count + " " + (n - 2));
 
 	}
 	public static long fib(int n) {
 		if(n <= 2){
+			count++;
 			return 1;
 		}else{
-			count++;
 			return fib(n - 1) + fib(n - 2);
 		}
 		}
