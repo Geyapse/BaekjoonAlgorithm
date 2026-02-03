2a5bc2b1eec21825e1ebdd1ac0df3793cc0f8277
Geyapse <113494802+Geyapse@users.noreply.github.com>
2026-02-03 21:06:35 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 156 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String s = in.nextLine();
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+
+			if (Character.isUpperCase(c)) {
+				sb.append(Character.toLowerCase(c));
+			} else {
+				sb.append(Character.toUpperCase(c));
+			}
+		}
+
+		System.out.println(sb.toString());
+	}
+}
